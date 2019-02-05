d80f49ac81c4d36b4de84cd872ac917fd9670249
Kim Nguyen <45023310+KimNguyen2018@users.noreply.github.com>
2019-02-04 20:09:05 -0500

    Chapter 2 Exersises

---
+++ Chapter2Exercise.java
@@ -0,0 +1,12 @@
+
+package chapter2exercise;
+public class Chapter2Exercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
