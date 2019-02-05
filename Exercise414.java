4feb9b177ba2f51ab546b2ac5ce4f7156fece3d5
Kim Nguyen <45023310+KimNguyen2018@users.noreply.github.com>
2019-02-04 20:11:00 -0500

    Chapter 4 Exersises

---
+++ Exercise414.java
@@ -0,0 +1,17 @@
+
+
+
+
+/* Author : Kim Ngoc Nguyen */
+
+
+
+public class Exercise414 {
+    public static void main(String[] args) {
+        int total =0;
+        for (int i=0; i<=20; total +=1, i +=2);
+            //total +=1;
+        System.out.println("total: " + total);
+    }
+    
+}
---
+++ Exercise416.java
@@ -0,0 +1,10 @@
+
+/* @author:  Kim Ngoc Nguyen */
+public class Exercise416 {
+   public static void main(String[] args) {
+       char randChar = (char) Math.random();
+       char cLetter = Character.toUpperCase(randChar);
+       System.out.println("Random uppercase letter: " + cLetter);
+    }
+    
+}
