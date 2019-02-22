353ccbf288e99e2cef4d4c4457274d5331381e98
Kim Nguyen <45023310+KimNguyen2018@users.noreply.github.com>
2019-02-22 15:58:26 -0500

    Faculty class extending from Employee class

---
+++ Faculty.java
@@ -0,0 +1,19 @@
+package chapter11;
+public class Faculty extends Employee {
+    int officeHours;
+    String rank;
+    
+    public Faculty(){};
+    
+    public Faculty(int officeHours, String rank){
+        this.officeHours = officeHours;
+        this.rank = rank;
+    }
+    
+    public String toString(){        
+        return ("Name: " + this.name +         
+               "\nPosition: " + this.rank + 
+                "\nOffice Hours : " + this.officeHours);
+    }    
+    
+}
