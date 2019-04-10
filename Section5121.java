6749b78b2906eeb84f63dd347cc31b78291f5244
Kim Nguyen <45023310+KimNguyen2018@users.noreply.github.com>
2019-04-10 08:27:30 -0400

    Letter - Random Character

---
+++ Section5121.java
@@ -0,0 +1,10 @@
+/* Author: Kim Ngoc Nguyen*/
+package section521;
+
+public class Section521 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
